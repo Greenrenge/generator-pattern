d7d9ef015ce1c5ff32dd6c4fe2fd39cb693d7b16
Sorasak Srirussamee <sorasak@rentspree.com>
2020-06-26 18:23:39 +0700

    Create chunk.js

---
+++ chunk.js
@@ -0,0 +1,25 @@
+const Promise = require("bluebird")
+
+const getChuck = size =>
+  async function*(iterator) {
+    while (true) {
+      let i = 0
+      const promises = []
+      while (i < size) {
+        promises.push(iterator.next())
+        i++
+      }
+      const values = await Promise.allSettled(promises)
+      const valid = values
+        .filter(v => v.isFulfilled())
+        .map(v => v.value().value)
+        .filter(v => !!v)
+      yield valid
+      if (valid.length !== size) break
+    }
+    // work
+  }
+
+module.exports = {
+  getChuck,
+}
