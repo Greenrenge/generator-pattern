a508a28ee033e6d1eeb3d99fe322a694f656560c
Sorasak Srirussamee <sorasak@rentspree.com>
2020-06-26 17:33:07 +0700

    Update fan-in.js

--- fan-in.js
+++ fan-in.js
@@ -2,29 +2,35 @@ const createFanIn = (...asyncIterators) => {
   const valuePool = []
   const generatorPool = []
   const pool = new Set()
+  let totalLive = asyncIterators.length
 
   const addSource = async function(iterator) {
     const promise = iterator.next()
+    pool.add(promise)
     promise
       .then(({ value, done }) => {
         valuePool.push(value)
         if (!done) generatorPool.push(iterator)
         pool.delete(promise)
-        return done
       })
       .catch(() => {
-        return true
+        pool.delete(promise)
       })
-    pool.add(promise)
   }
 
   return {
     async *fanInGenerator() {
       asyncIterators.forEach(a => addSource(a))
       while (true) {
-        const isDone = await Promise.race([...pool])
-        yield valuePool.shift()
-        if (!isDone) addSource(generatorPool.shift())
+        try {
+          await Promise.race([...pool])
+          yield valuePool.shift()
+          await addSource(generatorPool.shift())
+        } catch (error) {
+          console.log("Iterator has error occurs",error) // eslint-disable-line
+          totalLive--
+          if (totalLive <= 0) break
+        }
       }
     },
   }
